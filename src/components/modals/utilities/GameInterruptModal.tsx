2098391d6b444ce2bf99ec5193cfeecb48641934
Grant Cox <gcoxexcel@gmail.com>
2025-09-29 14:12:59 -0400

    Fix Flex param issue

--- src/components/modals/utilities/GameInterruptModal.tsx
+++ src/components/modals/utilities/GameInterruptModal.tsx
@@ -15,11 +15,11 @@ export default function GameInterruptModal({ isOpen, onClose, onContinue }: Game
 
     return (
         <BaseModal isOpen={isOpen} onClose={onClose}>
-            <Flex direction="column" gap="1.5rem" padding="1rem">
+            <Flex $direction="column" $gap="1.5rem" $padding="1rem">
                 <h2>Active Game Warning</h2>
                 <p>You have an active game of {gameState.activeGame?.title}. Continuing will turn off the emulator.</p>
                 
-                <Flex direction="row" gap="1rem" justifyContent="flex-end">
+                <Flex $direction="row" $gap="1rem" $justifyContent="flex-end">
                     <Button 
                         $variation="destructive"
                         onClick={onClose}
