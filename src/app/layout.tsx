ef193c3e1da75f57f41ef52462ed63578e63342f
Grant Cox <42614867+gcox32@users.noreply.github.com>
2026-03-22 15:06:17 -0400

    Add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from 'react';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { AuthProvider } from '../contexts/AuthContext';
 import { SettingsProvider } from "@/contexts/SettingsContext";
@@ -45,6 +46,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>
         <AuthProvider>
           <SettingsProvider>
